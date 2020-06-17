be8faa8de963bb82b25d66b7500647020720e4e6
dmdekf <dmdekf@gmail.com>
2020-06-17 20:03:08 +0900

    Add css

--- articles/views.py
+++ articles/views.py
@@ -11,16 +11,6 @@ from .forms import CommentForm
 from .permissions import IsOwnerOrReadOnly
 # Create your views here.
 
-# def create(self, request, *args, **kwargs):
-#     serializer = self.get_serializer(data=request.data)
-#     serializer.is_valid(raise_exception=True)
-#     self.perform_create(serializer)
-#     headers = self.get_success_headers(serializer.data)
-#     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-# def perform_create(self, serializer):
-#     serializer.save()
-#     return Response(serializer.data)
-
 
 class CommentViewset(viewsets.ModelViewSet):
     permission_classes = (
@@ -32,8 +22,8 @@ class CommentViewset(viewsets.ModelViewSet):
 class ArticleListViewset(viewsets.ModelViewSet):
     queryset = Article.objects.order_by('-pk')
     serializer_class = ArticleListSerializer
-    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-    #                       IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
 
 
 @api_view(['GET', 'POST'])
--- movies/forms.py
+++ movies/forms.py
@@ -3,7 +3,8 @@ from .models import Score
 
 
 class ScoreForm(forms.ModelForm):
-    score = forms.FloatField()
+    score = forms.FloatField(required=False, max_value=10, min_value=0,
+                             label="점수를 입력해주세요", widget=forms.NumberInput(attrs={'placeholder': '10.0'}))
 
     class Meta:
         model = Score
